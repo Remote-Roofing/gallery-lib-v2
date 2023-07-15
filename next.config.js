77e01e1283b879355d17b9455aa61fe8a01238d5
HooriaHIC <hooria.hic@gmail.com>
2023-07-16 01:34:39 +0500

    fix: webpack configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+      config.module.rules.push({
+        test: /\.ts$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            compilerOptions: {
+              noEmit: false,
+            },
+          },
+        },
+      });
+      
+      return config;
+    },
     experimental: {
       appDir: true,
     },
