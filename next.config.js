de253ef704ace56ccf8e7ab05496f0485c926bab
HooriaHIC <hooria.hic@gmail.com>
2023-07-16 02:08:36 +0500

    testing

--- next.config.js
+++ next.config.js
@@ -1,26 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-      config.module.rules.push({
-        test: /\.ts$/,
-        use: {
-          loader: 'ts-loader',
-          options: {
-            compilerOptions: {
-              noEmit: false,
-            },
-          },
-        },
-      });
-      
-      return config;
-    },
     experimental: {
       appDir: true,
     },
     images: {
       domains: ["loremflickr.com", "res.cloudinary.com"],
+    },
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+      config.module.rules.push({
+        test: /\.tsx?$/, // For .ts and .tsx files
+        exclude: /node_modules/,
+        use: {
+            loader: "ts-loader",
+            options: {
+              compilerOptions: {
+                noEmit: false,
+              },
+            },
+        },
+      });
+
+      return config;
     },
   }
   
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 import Modal from "./components/modal";
 import SharedModal from "./components/sharedModal";
 
-export { Modal, SharedModal };
+import { variants } from "./utilities/animationVariants";
+import downloadPhoto from "./utilities/downloadPhoto";
+import getBase64ImageUrl from "./utilities/generateBlurPlaceholder";
+import { range } from "./utilities/range";
+import { useLastViewedPhoto } from "./utilities/useLastViewedPhoto";
+
+export { Modal, SharedModal, variants, downloadPhoto, getBase64ImageUrl, range, useLastViewedPhoto };
