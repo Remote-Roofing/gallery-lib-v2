939573f177e4bccea804db25674b650f00071390
HooriaHIC <hooria.hic@gmail.com>
2023-07-16 02:26:08 +0500

    removed getPlaceHolderURL

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,7 @@ import SharedModal from "./components/sharedModal";
 
 import { variants } from "./utilities/animationVariants";
 import downloadPhoto from "./utilities/downloadPhoto";
-import getBase64ImageUrl from "./utilities/generateBlurPlaceholder";
 import { range } from "./utilities/range";
 import { useLastViewedPhoto } from "./utilities/useLastViewedPhoto";
 
-export { Modal, SharedModal, variants, downloadPhoto, getBase64ImageUrl, range, useLastViewedPhoto };
+export { Modal, SharedModal, variants, downloadPhoto, range, useLastViewedPhoto };
